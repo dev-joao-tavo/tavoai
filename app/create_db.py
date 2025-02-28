364815bb811de78e9878f139bcd3b8598f3ae17f
dev-joao-tavo <chatbotsndn@gmail.com>
2025-02-28 17:26:35 -0300

    db

--- app/create_db.py
+++ app/create_db.py
@@ -3,9 +3,6 @@ from db import engine, Base
 
 async def init_db():
     async with engine.begin() as conn:
-        print("Dropping existing tables if they exist...")
-        await conn.run_sync(Base.metadata.drop_all)  # Drop existing tables
-        print("Creating tables...")
         await conn.run_sync(Base.metadata.create_all)  # Create new tables
         print("✅ Database tables created successfully!")
 
