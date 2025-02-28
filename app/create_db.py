8858cd0303e107d50cc6752403dd87c21cf91235
dev-joao-tavo <chatbotsndn@gmail.com>
2025-02-28 17:29:28 -0300

    db

--- app/create_db.py
+++ app/create_db.py
@@ -3,8 +3,7 @@ from db import engine, Base
 
 async def init_db():
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)  # Create new tables
-        print("✅ Database tables created successfully!")
+        await conn.run_sync(Base.metadata.create_all)
 
 if __name__ == "__main__":
     asyncio.run(init_db())
