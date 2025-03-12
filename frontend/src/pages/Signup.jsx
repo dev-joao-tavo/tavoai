56b8a928e3a438ff7b483f9acc384a57afc4291d
dev-joao-tavo <chatbotsndn@gmail.com>
2025-03-12 09:05:20 -0300

    phone

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import "./Login.css"; // Reuse Login.css
 
-const Signup = () => {phone
+const Signup = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [phone, setPhone] = useState('');
