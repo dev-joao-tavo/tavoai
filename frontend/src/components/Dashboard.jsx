8d6cf87001b4be3ca2c3a2b3e62c2548f50fdc48
dev-joao-tavo <chatbotsndn@gmail.com>
2025-03-13 09:03:32 -0300

    assinatura

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -210,7 +210,7 @@ const Dashboard = () => {
     <div className="dashboard-container">
       <div className="header-container">
         <button onClick={() => window.location.href = "/dashboard"} className="header-button">Dashboard</button>
-        <button onClick={() => window.location.href = "/subscription"} className="header-button">Subscription</button>
+        <button onClick={() => window.open("https://pay.infinitepay.io/tavoai/Ri0x-1HOAcj6R35-19,90", "_blank")} className="header-button"> Assinatura </button>
         <button onClick={handleLogout} className="header-button">Logout</button>
       </div>
 
