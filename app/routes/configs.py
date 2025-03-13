7eb6a2bc965d7025539132906347c5b40003622b
dev-joao-tavo <chatbotsndn@gmail.com>
2025-03-13 07:59:40 -0300

    chrome_options.add_argument("--headless=new")

--- app/routes/configs.py
+++ app/routes/configs.py
@@ -7,14 +7,13 @@ import os
 
 def initialize_driver(user_id):
     chrome_options = Options()
-    # Path to Chrome user data (macOS)
-    chrome_user_dir = "/Users/usuario/Library/Application Support/Google/Chrome"
-    
+    # Path to Chrome user data (macOS)    
     PROFILE_BASE_DIR = "/tmp/chrome_profiles"  # Base directory for profiles
 
     # Define a persistent profile directory for each user
     user_profile_dir = os.path.join(PROFILE_BASE_DIR, f"user_{user_id}")
     chrome_options.add_argument(f"--user-data-dir={user_profile_dir}")
+    chrome_options.add_argument("--headless=new")
 
     chrome_options.add_argument(f"--profile-directory=Profile {user_id}")  # Adjust to your active profile
 
